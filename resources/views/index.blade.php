787bdc95de1c22a91598899cfacac9c3b643ec3f
bijoy <mofizuldev@gmail.com>
2026-02-26 19:02:19 +0600

    Random Little Change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,13 @@
     
     <div class="container p-5">
       <h1>API T|E|S|T|I|N|G</h1>
+      <hr>
+      <div>
+        <H1>Hello!</H1>
+      </div>
     </div>
+    
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
   </body>
 </html>
